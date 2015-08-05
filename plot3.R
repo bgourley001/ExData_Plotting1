0c863fda9991e5fed312da291f0db41cc85544bc
Bill Gourley <bgourley001@gmail.com>
2015-08-05 18:01:42 +0100

    Corrected typo

--- plot3.R
+++ plot3.R
@@ -86,7 +86,7 @@ with(plot3.data,lines(date.times,Sub_metering_2,type = "l",col="red"))
 with(plot3.data,lines(date.times,Sub_metering_3,type = "l",col="blue"))
 legend(x = "topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty = c(1,1),col = c("black","red","blue"))
 
-#create png plot (plot2.png)
+#create png plot (plot3.png)
 png(filename = "plot3.png",width = 480,height = 480)
 with(plot3.data,plot(date.times,Sub_metering_1, type = "l",xlab = "",ylab = "Energy sub metering"))
 with(plot3.data,lines(date.times,Sub_metering_2,type = "l",col="red"))
